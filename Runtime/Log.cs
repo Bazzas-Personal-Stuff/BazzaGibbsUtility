fd5895f12d89a9ddf9e04071094907ecf63f1bfd
Bazzagibbs <bailey.gibbons2018@gmail.com>
2023-05-06 12:58:55 +1000

    Added context overloads to Log

--- Runtime/Log.cs
+++ Runtime/Log.cs
@@ -8,20 +8,39 @@ namespace BazzaGibbs.Utility
     /// </summary>
     public static class Log {
         
+        // Info
         [Conditional("DEVELOPMENT_BUILD"), Conditional("UNITY_EDITOR")]
         public static void Info(object message) {
             Debug.Log(message);
         }
+        
+        [Conditional("DEVELOPMENT_BUILD"), Conditional("UNITY_EDITOR")]
+        public static void Info(object message, UnityEngine.Object context) {
+            Debug.Log(message, context);
+        }
 
+        
+        // Warn
         [Conditional("DEVELOPMENT_BUILD"), Conditional("UNITY_EDITOR")]
         public static void Warn(object message) {
             Debug.LogWarning(message);
         }
 
         [Conditional("DEVELOPMENT_BUILD"), Conditional("UNITY_EDITOR")]
+        public static void Warn(object message, UnityEngine.Object context) {
+            Debug.LogWarning(message, context);
+        }
+        
+        
+        // Error
+        [Conditional("DEVELOPMENT_BUILD"), Conditional("UNITY_EDITOR")]
         public static void Error(object message) {
             Debug.LogError(message);
         }
         
+        [Conditional("DEVELOPMENT_BUILD"), Conditional("UNITY_EDITOR")]
+        public static void Error(object message, UnityEngine.Object context) {
+            Debug.LogError(message, context);
+        }
     }
 }
